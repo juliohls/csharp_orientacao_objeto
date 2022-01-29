cd3a2969e9c76e3044e59ee18cd2b1cb91c03b18
Julio Henrique de Lima Santos <julio.h.santos@animaeducacao.com.br>
2022-01-29 16:04:58 -0300

    Aprendendo métodos e funções

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aprendendo metodos e funções
+            ContaCorrente contaBruno = new ContaCorrente();
+
+            contaBruno.titular = "Bruno";
+
+            Console.WriteLine("Saldo: R$" + contaBruno.saldo);
+            Console.WriteLine("Sacando..");
+            contaBruno.Sacar(50);
+            Console.WriteLine("Saque feito!");
+            Console.WriteLine("Saldo restante: R$" + contaBruno.saldo);
+
+
+            contaBruno.Depositar(500);
+            Console.WriteLine(contaBruno.saldo);
+
+
+            ContaCorrente contaGabriela = new ContaCorrente();
+
+            contaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo Bruno: R$" + contaBruno.saldo);
+            Console.WriteLine("Saldo Gabriela: R$" + contaGabriela.saldo);
+            contaBruno.Transferir(200, contaGabriela);
+            Console.WriteLine("Saldo Bruno: R$" + contaBruno.saldo);
+            Console.WriteLine("Saldo Gabriela: R$" + contaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
