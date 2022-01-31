7142fdd8de607903ec18d5a1033e8f2235a8b891
Julio Henrique de Lima Santos <julio.h.santos@animaeducacao.com.br>
2022-01-31 11:28:52 -0300

    Aprendendo importância de namespaces parte 1

---
+++ 05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank // NameSpaces são importantes pra deixar o projeto dentro do mesmo domínio de classes
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int conta;
+        public double saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank // NameSpaces são importantes pra deixar o projeto dentro do mesmo domínio de classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente();
+
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "441.041.158.60";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.conta = 563456;
+
+            conta.titular.nome = "Gabriela Costa"; // Altera nos 2 objetos, pois estão na mesma referência, tanto pela conta, como pelo cliente.
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
